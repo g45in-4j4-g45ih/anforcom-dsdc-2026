8fe209698061e5c791d94cc77776444a80354540
namarabigail <bigelnp10@gmail.com>
2026-08-21 10:18:15 +0700

    refactor(locations): extract Location model to its own app

--- backend/locations/models.py
+++ backend/locations/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    alamat = models.TextField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+
+    def __str__(self):
+        return f"Lokasi: {self.alamat}"
