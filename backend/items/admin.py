a84e8e301e0f993544299537799872a2c3605d48
namarabigail <bigelnp10@gmail.com>
2026-08-22 18:36:24 +0700

    fix: register Store, Item, ItemImage, Klaim, Location ke Django admin

--- backend/items/admin.py
+++ backend/items/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Store, Item, ItemImage, Klaim, Location
 
-# Register your models here.
+admin.site.register(Store)
+admin.site.register(Item)
+admin.site.register(ItemImage)
+admin.site.register(Klaim)
+admin.site.register(Location)
